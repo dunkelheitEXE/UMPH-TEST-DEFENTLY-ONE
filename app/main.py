f9deedade7595107b90a61ea036b2504d145cd3a
Alfred0JHH <alfre.hdez.hdez.ajhh@hmail.com>
2023-01-30 16:31:19 -0600

    Objetivos

--- app/main.py
+++ app/main.py
@@ -73,11 +73,13 @@ words = [
     "Hablame sobre Ingeneria en Tecnologías de la información","El objetivo es formar profesionales conscientes de su responsabilidad ética y social, competentes para el análisis de necesidades tecnológicas en las organizaciones, y el diseño, desarrollo e implementación de soluciones basadas en el uso de las TIC en las nuevas tendencias que se requieren en la actualidad como la Industria 4.0"
     #Objetivos de las licenciaturas
     "Hablame sobre la Licenciatura en Administración y Gestión Empresarial",
-    ""
-    "",""
-    "",""
-    "",""
+    "El objetivo es diseñar y coordinar la operación de las empresas, evaluar su modelo empresarial y lograr metas, innovación y competitividad. Se usarán diagnósticos organizacionales, procesos administrativos, manejo de recursos financieros y tecnológicos, planeación estratégica, estándares de calidad, marco legal y normatividad. La finalidad es garantizar productos y servicios de calidad, elevar rentabilidad y expansión de la organización, y contribuir al desarrollo sostenible y al crecimiento económico y social."
+    "Hablame dobre la Licenciatura en Arquitectura Bioclimática","Formar profesionales íntegros, comprometidos con el ambiente y la sociedad, capaces de desarrollar proyectos arquitectónicos contemporáneos bioclimáticamente sostenibles y competitivos, considerando los reglamentos y la normatividad vigente."
+    "Hablane sobre la Licenciatura en Comercio Internacional y Aduanas","La mision es formar personas íntegras, con competencias, capacidades y habilidades de clase mundial; mediante la docencia, investigación, operación y promoción del comercio internacional y aduanas, que contribuyan al desarrollo sostenible de México, para satisfacer las necesidades y expectativas de los sectores público, privado y social."
     #Objetivos de las mestrias
+    "Hablame sobre la Maestría en Comercio y Logística Internacional","Busca desarrollar profesionales que entiendan la complejidad de la globalización, para mejorar el intercambio comercial de México, atendiendo las necesidades de las empresas para su internacionalización, fortaleciendo la distribución de las mercancías y el cumplimiento del marco regulatorio de la materia."
+    "Hablame sobre la Maestría en Ingeniería Aeroespacial","Tiene como objetivo general el especializar eficientemente a profesionistas en el ámbito académico e industrial nacional e internacional en materia aeroespacial, a fin del cumplimiento de dicho objetivo la especialización consta de dos áreas fundamentales también llamadas como Líneas de Generación y Aplicación del Conocimiento"
+    "Hablame sobre la Maestría en Inteligencia Artificial","Busca formar profesionales en la implementación de técnicas, métodos y algoritmos de inteligencia artificial orientados la optimización y automatización de procesos aplicados tanto a la ingeniería, como al análisis de datos e inteligencia de negocios."
     ]
 #sdsa
 
